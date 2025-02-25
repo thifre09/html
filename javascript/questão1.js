0236911844b1136949b5a73920b0cd8efd5b0fcf
thifre09 <128859182+thifre09@users.noreply.github.com>
2025-02-25 09:11:00 -0300

    algo

--- javascript/questão1.js
+++ javascript/questão1.js
@@ -2,6 +2,6 @@ let b1 = 70;
 let b2 = 50;
 let b3 = 100;
 let b4 = 90;
-let media = (b1+b2+b3+b4)/4
+let media = ((b1*2)+(b2*2)+(b3*3)+(b4*3))/4
 
 console.log(media)
--- javascript/questão2.js
+++ javascript/questão2.js
@@ -1,4 +1,4 @@
-let c = 10;
+let c = Number(window.prompt("Digite o capital inicial"));
 let taxaJuros = 0.05;
 let meses = 240;
 let m = c * ((1+taxaJuros) ** 240)
